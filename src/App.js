1982da0906e277a203fb2609d6a0998b0821e0a0
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2021-04-28 12:22:24 +1200

    Added background image to homepage
    I added in a background image as well as a header and some text.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import hcbgImage from "./books-1655783.jpg";
 
 // function App() {
 //   return (
@@ -50,6 +50,7 @@ export default function App() {
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#getting-started">Getting Started</Nav.Link>
+            <Nav.Link href="#faq-and-help">FAQ & Help</Nav.Link>
             <Nav.Link href="#about-us">About Us</Nav.Link>
             <NavDropdown title="Dropdown">
               <NavDropdown.Item href="#item/1">Item 1</NavDropdown.Item>
@@ -63,10 +64,25 @@ export default function App() {
             <FormControl type="text" placeholder="search" className="mr-sm-2" />
             <Button className="navbar-button-color" onClick={ searchButtonClicked }>search</Button>
           </Form>
-          <Button className="navbar-button-color">Login</Button>
-          <Button className="navbar-button-color">Sign Up</Button>
+          <Button className="navbar-button-color" onClick={ loginButtonClicked }>Login</Button>
+          <Button className="navbar-button-color" onClick={ signUpButtonClicked }>Sign Up</Button>
         </Navbar.Collapse>
       </Navbar>
+      <div 
+        class="bg_img"
+        style={{
+          backgroundImage: 'url('+hcbgImage+')',
+          backgroundSize: "cover",
+          height: "100vh",
+          color: "#f5f5f5"
+        }}
+      >
+      <h1 className="welcome-header-home">Welcome to SEED</h1>
+      <p className="Purpose-p"><b>Purpose: </b>The purpose of this website it to allow for practitionars to look up articles from
+      our database. <br />First off you should vist the "Getting Started" page. This will walk you through
+      how to use our services. <br />If you are still stuck please vist our "FAQ & Help" page.
+      </p>
+      </div>
       {/* <Nav variant="tabs" defaultActiveKey="/home"> */}
         {/* navbar using tabs */}
   {/* <Nav.Item>
@@ -103,4 +119,14 @@ export default function App() {
 function searchButtonClicked()
 {
   alert("Search button pressed, will take input from search field and search (future).");
+}
+
+function signUpButtonClicked()
+{
+  alert("Sign up button pressed, will show overlayed form of the sign up (future).");
+}
+
+function loginButtonClicked()
+{
+  alert("Log in button pressed, will show overlayed form for the login (future).");
 }
