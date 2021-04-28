686579c92d5d0b4d04a0c9574af29df49745b60f
XOneTrick <68628946+XOneTrick@users.noreply.github.com>
2021-04-29 11:36:16 +1200

    Connection to mongoDB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const express = require("express");
+
+  const app = express();
+
+  app.get("/", (req, res) => res.send("Hello world!"));
+
+  const port = process.env.PORT || 8082;
+
+  app.listen(port, () => console.log(`Server running on port ${port}`));
 }
 
 export default App;
