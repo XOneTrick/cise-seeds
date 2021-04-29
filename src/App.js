1c03f13fc51962b8ebd1da0de2471a70f4b04d92
XOneTrick <68628946+XOneTrick@users.noreply.github.com>
2021-04-29 12:29:36 +1200

    Revert "Connection to mongoDB"
    This reverts commit 686579c92d5d0b4d04a0c9574af29df49745b60f.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const express = require("express");
-
-  const app = express();
-
-  app.get("/", (req, res) => res.send("Hello world!"));
-
-  const port = process.env.PORT || 8082;
-
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
