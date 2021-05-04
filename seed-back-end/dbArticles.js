679447a889f85c25710a7943177218e6f4a0fec6
XOneTrick <68628946+XOneTrick@users.noreply.github.com>
2021-05-04 14:35:08 +1200

    added more formats

--- seed-back-end/dbArticles.js
+++ seed-back-end/dbArticles.js
@@ -3,6 +3,12 @@ import mongoose from "mongoose";
 const articleSchema = mongoose.Schema({
   title: String,
   author: String,
+  semethod: String,
+  researchmethod: String,
+  journal: String,
+  article: String,
+  publishdate: Number,
+  year: Number,
 });
 
 export default mongoose.model("articles", articleSchema);
