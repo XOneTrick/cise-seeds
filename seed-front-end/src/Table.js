cde634f6d53ed655f45bdb750051958d07397f4e
coolo-droid <70293739+coolo-droid@users.noreply.github.com>
2021-05-20 15:08:25 +1200

    Done some more testing
    Worked out how to do the testing with the expect() function and also worked out a more appropriate way to export functions and then import them into another class/file.

---
+++ seed-front-end/src/Table.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Table()
+{
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>table heading</p>
+          </header>
+        </div>
+      );
+}
+
+function makeTable()
+{
+    return "a";
+}
+
+
+export {Table, makeTable };
--- seed-front-end/src/Table.test.js
+++ seed-front-end/src/Table.test.js
@@ -1,19 +1,24 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { Table, makeTable } from "./Table";
 import ReactDOM from "react-dom";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Table />);
+  const linkElement = screen.getByText(/table heading/i);
   expect(linkElement).toBeInTheDocument();
 });
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Table />, div);
 });
 
 test("string matches", () =>
 {
   expect("a").toBe("a");
 });
+
+test("make table", () =>
+{
+    expect(makeTable()).toBe("b");
+})
